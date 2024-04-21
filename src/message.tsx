530d15d069e836a1b656fe3eff7d1cae91c234cd
IbrahimaTk-7 <ibrahimatounkangbalde@gmail.com>
2024-04-21 06:34:28 +0000

    Creating a React Component

---
+++ src/message.tsx
@@ -0,0 +1,11 @@
+//Pascal case
+
+function Message() {
+    //JSX: JavaScript XML
+    const name = 'Ibrahima';
+    if(name)
+        return <h1>Hello {name}</h1>;
+    return <h1>hello world</h1>   
+};
+
+export default Message;
